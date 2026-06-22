008eda3b90e80d8d555f0369100f483c5df4d2e0
Steve Forde <sforde08@gmail.com>
2026-06-22 03:15:39 +0100

    Move app content into src/components/Main structure

--- App.js
+++ App.js
@@ -1,23 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Main from "./src/components/Main";
 
-export default function App() {
-  // Put it right here:
-  console.log("Testing my connection!");
+const App = () => {
+  return <Main />;
+};
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
