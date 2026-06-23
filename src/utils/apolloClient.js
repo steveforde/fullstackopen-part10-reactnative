95bf24936d3359a89e392ec0cf05ef687832901f
Steve Forde <sforde08@gmail.com>
2026-06-23 21:16:47 +0100

    Complete environment variable integration for Apollo Client using HttpLink

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,12 +1,10 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-
-const httpLink = new HttpLink({
-  uri: "http://localhost:4000/graphql",
-});
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const createApolloClient = () => {
   return new ApolloClient({
-    link: httpLink,
+    link: new HttpLink({
+      uri: process.env.EXPO_PUBLIC_APOLLO_URI,
+    }),
     cache: new InMemoryCache(),
   });
 };
